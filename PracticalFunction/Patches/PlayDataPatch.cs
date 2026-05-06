53f7a3bff6df53f047ed063227a39694de896165
Lan <102645937+LanVVVVV@users.noreply.github.com>
2026-05-06 23:33:21 +0800

    Bugfix: Bug fix: Prevented the loss of Achievement Points that should have been awarded for modifying starting gold.

---
+++ PracticalFunction/Patches/PlayDataPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using MBMScripts;
+
+namespace PracticalFunction.Patches;
+
+[HarmonyPatch(typeof(PlayData))]
+public class PlayDataPatch
+{
+
+    [HarmonyPatch(nameof(PlayData.AchievementPoint), MethodType.Setter)]
+    [HarmonyPrefix]
+    public static bool AchievementPointPrefix(int value)
+    {
+        if (value == 0 && !GameManager.Instance.PlayerData.GetGameEventFlag(EGameEvent.SavePoint_10000000)) return false;
+        return true;
+    }
+}
